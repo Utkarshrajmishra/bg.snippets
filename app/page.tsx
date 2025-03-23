e7531c3188e66adfab9ca3bd5b81267b05c03d50
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-03-23 20:09:53 +0530

    Inital Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,143 @@
-import Image from "next/image";
+"use client";
+import { ArrowRight, Github, Twitter } from "lucide-react";
+import { BACKGROUND_OPTIONS } from "@/components/background";
+import { Button } from "@/components/ui/button";
+import Playground from "@/components/playground";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [preview, setPreview] = useState<null | React.ReactNode>(null);
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const resetBg = () => {
+    setPreview(null);
+    setTheme("light");
+  };
+
+  return (
+    <>
+      <div className={`${theme}`}>
+        <div className="fixed font-inter left-0 top-0 -z-10 h-full w-full">
+          {preview ? preview : null}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="relative mx-auto h-screen w-full max-w-7xl px-6 md:px-8 lg:px-12">
+          <header className="flex items-center justify-between py-8">
+            <div />
+            <nav className="flex gap-6">
+              <a
+                href="https://twitter.com/Ibelick"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex"
+              >
+                <Twitter
+                  className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
+                  strokeWidth={1.5}
+                />
+              </a>
+              <a
+                href="https://github.com/ibelick/background-snippets"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex"
+              >
+                <Github
+                  className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
+                  strokeWidth={1.5}
+                />
+              </a>
+            </nav>
+          </header>
+          <div className="pt-8">
+            <div className="relative mx-auto flex max-w-2xl flex-col items-center">
+              <div className="mb-8 flex">
+                <a
+                  href="https://github.com/ibelick/background-snippets"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex"
+                >
+                  <span className="relative inline-block overflow-hidden rounded-full p-[1px]">
+                    <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#a9a9a9_0%,#0c0c0c_50%,#a9a9a9_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#171717_0%,#737373_50%,#171717_100%)]" />
+                    <div className="inline-flex h-full w-full cursor-pointer justify-center rounded-full bg-white px-3 py-1 text-xs font-medium leading-5 text-slate-600 backdrop-blur-xl dark:bg-black dark:text-slate-200">
+                      New snippets ⚡️
+                      <span className="inline-flex items-center pl-2 text-black dark:text-white">
+                        Read more{" "}
+                        <ArrowRight
+                          className="pl-0.5 text-black dark:text-white"
+                          size={16}
+                        />
+                      </span>
+                    </div>
+                  </span>
+                </a>
+              </div>
+              <h2 className="text-center text-3xl font-medium text-gray-900 dark:text-gray-50 sm:text-6xl">
+                Collection of modern,{" "}
+                <span className="animate-text-gradient inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text leading-tight text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">
+                  background snippets
+                </span>
+              </h2>
+              <p className="mt-6 text-center text-lg leading-6 text-gray-600 dark:text-gray-200">
+                Ready-to-use, simply copy and paste into your next project. All
+                snippets crafted with Tailwind CSS and{" "}
+                <span className="cursor-wait opacity-70">Vanilla CSS</span>.
+              </p>
+              <div className="mt-10 flex gap-4">
+                <a
+                  href="https://github.com/ibelick/background-snippets"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center"
+                >
+                  <Button>
+                    Go to GitHub <ArrowRight className="pl-0.5" size={16} />
+                  </Button>{" "}
+                </a>
+                <Button variant="secondary" onClick={resetBg}>
+                  Reset background
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden px-4 pb-20 pt-52 md:px-10">
+            <div className="grid grid-cols-1 gap-6 pb-6 md:grid-cols-2 lg:grid-cols-4">
+              {BACKGROUND_OPTIONS.map((background, index) => {
+                return (
+                  <Playground
+                    key={index}
+                    setPreview={setPreview}
+                    theme={background.theme}
+                    setTheme={setTheme}
+                  >
+                    {background.component}
+                  </Playground>
+                );
+              })}
+            </div>
+            <div className="inline-flex max-w-2xl rounded-md border border-neutral-200 bg-white p-2 text-sm text-neutral-900 dark:border-neutral-900 dark:bg-black dark:text-neutral-200">
+              {`These backgrounds are made for a full page background. The preview
+              can be different from the actual result. Click on preview to test
+              it. And don't forget to tweak it to your needs.`}
+            </div>
+          </div>
+          <footer>
+            <div className="flex items-center justify-center py-8">
+              <span className="text-sm text-neutral-800 dark:text-neutral-200">
+                Made by
+                <a
+                  href="https://twitter.com/Ibelick"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-1 text-neutral-950 dark:text-neutral-100"
+                >
+                  @Ibelick
+                </a>
+              </span>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
